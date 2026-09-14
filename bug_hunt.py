cc7132ce411bdb0da0e463999c7cc0f74c2bcf3a
45Keach <jockchuolkeach@gmail.com>
2026-09-14 16:29:51 +0300

    Document and fix all three bug hunt bugs

--- bug_hunt.py
+++ bug_hunt.py
@@ -2,6 +2,7 @@ count = 1
 total = 0
 
 # BUG: The while statement was missing a colon, so I added one.
+# BUG: The condition stopped at 4 because it used < 5, so I changed it to <= 5 to include 5.
 while count <= 5:
     total = total + count
     count = count + 1
